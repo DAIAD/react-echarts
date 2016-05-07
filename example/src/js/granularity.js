3c31c130e99e51b15ea224c0d9c27d1cd64c0c1a
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2016-05-08 01:42:22 +0300

    Rewrite granularity module in a similar way to timespan.

--- example/src/js/granularity.js
+++ example/src/js/granularity.js
@@ -2,11 +2,10 @@
 
 var moment = require('moment');
 
-var Granularity = function (name, quantity, unit)
+var Granularity = function (quantity, unit)
 {
   this.unit = unit; // same as in Moment.js
   this.quantity = quantity;
-  this.name = name;
 }
 
 Granularity.prototype.toDuration = function ()
@@ -19,32 +18,24 @@ Granularity.prototype.valueOf = function ()
   return this.toDuration().asMilliseconds();
 }
 
-Granularity.MILLISECOND = new Granularity('millisecond', 1, 'ms');
-Granularity.SECOND = new Granularity('second', 1, 's');
-Granularity.MINUTE = new Granularity('minute', 1, 'm');
-Granularity.HOUR = new Granularity('hour', 1, 'h');
-Granularity.DAY = new Granularity('day', 1, 'd');
-Granularity.WEEK = new Granularity('week', 1, 'w');
-Granularity.MONTH = new Granularity('month', 1, 'M');
-Granularity.QUARTER = new Granularity('quarter', 1, 'Q');
-Granularity.YEAR = new Granularity('year', 1, 'y');
+// A (sorted descending) map of commonly used instances 
+Granularity.common = new Map([
+  ['millisecond', {args: [1, 'ms']}],
+  ['second', {args: [1, 's']}],
+  ['minute', {args: [1, 'm']}],
+  ['hour', {args: [1, 'h']}],
+  ['day', {args: [1, 'd']}],
+  ['week', {args: [1, 'w']}],
+  ['month', {args: [1, 'M']}],
+  ['quarter', {args: [1, 'Q']}],
+  ['year', {args: [1, 'y']}],
+])
 
-// A (sorted descending) list of commonly used instances 
-Granularity.common = [
-  Granularity.MILLISECOND,
-  Granularity.SECOND,
-  Granularity.MINUTE,
-  Granularity.HOUR,
-  Granularity.DAY,
-  Granularity.WEEK,
-  Granularity.MONTH,
-  Granularity.QUARTER,
-  Granularity.YEAR,
+Granularity.fromName = (name) => {
+  var u = Granularity.common.get(name);
+  return !u? null : (new Granularity(...u.args));
+};
 
-];
-
-Granularity.fromName = (name) => (Granularity.common.find(g => (g.name == name)));
-
-Granularity.names = () => (Granularity.common.map(g => g.name))
+Granularity.names = () => (Array.from(Granularity.common.keys()));
 
 module.exports = Granularity;
